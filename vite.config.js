c9ccca2092cb1938f1344e17a766d62558df6de1
McWise02 <micah02.brown@gmail.com>
2025-08-14 22:00:03 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         plan: resolve(__dirname, "src/create_plan/index.html"),
-        // checkout: resolve(__dirname, "src/checkout/index.html"),
-        // product: resolve(__dirname, "src/product_pages/index.html"
-        // ),
+        view: resolve(__dirname, "src/view_plans/index.html"),
+        random: resolve(__dirname, "src/random_activity/index.html"),
         // products: resolve(__dirname, "src/product_listing/index.html"),
         // login: resolve(__dirname, "src/login/index.html"),
         // register: resolve(__dirname, "src/register/index.html"),
